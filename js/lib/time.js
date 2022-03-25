1c4074856638d9752edd83b6a4cdef5b4c185ba3
Morgini-art <81078267+Morgini-art@users.noreply.github.com>
2022-03-25 12:35:56 +0100

    Add files via upload

---
+++ js/lib/time.js
@@ -0,0 +1,35 @@
+class Timer {
+	generalGameTime = 0;
+	listOfTicks = [];
+	
+	checkTheTickTime() {
+		for (let i = 0; i === this.listOfTicks.length; i++) {
+			if(this.listOfTicks[i].endTime == generalGameTime) {
+				this.listOfTicks[i].done = true;
+				console.log('The Tick Has Be End: '+this.listOfTicks[i]);
+			}
+		}
+		
+		this.generalGameTime++;
+	}
+}
+
+class Tick {
+	constructor(startTime,endTime,done = false) {
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.done = done;
+	}
+}
+
+const generalTimer = new Timer();
+console.log(generalTimer);
+
+function loop() {
+	generalTimer.checkTheTickTime();
+	generalTimer.generalGameTime++;
+}
+
+setInterval(loop, 1);
+
+generalTimer.listOfTicks.push(new Tick(20,40));
